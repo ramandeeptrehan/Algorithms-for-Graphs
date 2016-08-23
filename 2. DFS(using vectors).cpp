07ab9e130d07cf00f3c44681708db4e13882b8de
Ramandeep Singh Trehan <ramandeeptrehan@gmail.com>
2016-08-23 11:51:08 +0530

    Add files via upload

---
+++ 2. DFS(using vectors).cpp
@@ -0,0 +1,94 @@
+//
+//  DFS(using vector).cpp
+//  2. DFS
+//
+//  Created by Ramandeep Singh Trehan on 23/08/16.
+//  Copyright © 2016 Ramandeep Singh Trehan. All rights reserved.
+//
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void inputGraph(int numberOfEdges, vector<vector<pair<int, int> > > & adjList)
+{
+    int u, v, weight;
+    
+    for(int i=0; i<numberOfEdges; i++)
+    {
+        cout<<"Enter u and v (u-->v)\n";
+        cin>>u>>v;
+        cout<<"Enter weight of edge: \n";
+        cin>>weight;
+        adjList[u].push_back(make_pair(v, weight));
+    }
+    
+}
+
+
+void printGraph(int numberOfNodes, int numberOfEdges, vector<vector<pair<int, int> > > & adjList)
+{
+    for(int i=1; i<=numberOfNodes; i++)
+    {
+        for(int j=0; j<adjList[i].size(); j++)
+        {
+            cout<<"Edge is: " <<i<< "-->" << adjList[i][j].first<<endl;
+            cout<<"Weight is: "<< adjList[i][j].second<<endl;
+            
+        }
+    }
+}
+
+void DFSUtil(int s, vector<vector<pair<int, int> > > & adjList, bool * visited)
+{
+    visited[s]=true;
+    cout<<s<<" ";
+    
+    for(int i=0; i<adjList[s].size(); i++)
+    {
+        if(visited[adjList[s][i].first]==false)
+            DFSUtil(adjList[s][i].first, adjList, visited); //note: in u->v...passing the v
+    }
+
+}
+
+void DFS(int s, vector<vector<pair<int, int> > > & adjList, int n) // n is number of nodes in graph
+{
+    bool * visited = new bool[n+1]; //vertices from 1 to n
+    
+    for(int i=1; i<=n; i++)
+    {
+        visited[i]=false;
+    }
+    
+    DFSUtil(s, adjList, visited);
+    
+}
+
+int main()
+{
+    int n, e, s;
+    cout<<"Enter the number of nodes(n) and edges(e) you want in the graph: \n";
+    cin>>n>>e;
+    vector<vector<pair<int, int>>> adjList(n+1); //will access n nodes from 1 to n
+    
+    inputGraph(e, adjList);
+    
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=0; j<(int)adjList[i].size(); j++)
+        {
+            cout<<"Edge is: " <<i<< "-->" << adjList[i][j].first<<endl;
+            cout<<"Weight is: "<< adjList[i][j].second<<endl;
+            
+        }
+    }
+    cout<<"Printing the graph: "<<endl;
+    printGraph(n, e, adjList);
+    
+    cout<<"Enter the vertex(1 to n) as a starting vertex for DFS: \n";
+    cin>>s;
+    DFS(s, adjList, n);
+    
+}
